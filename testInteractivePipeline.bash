28a4621136bb483564d81c44487fa6ec64db1d0f
Matt Johnson <matthew.j@wustl.edu>
2022-01-10 12:09:25 -0600

    interactive test fixes

--- testInteractivePipeline.bash
+++ testInteractivePipeline.bash
@@ -5,7 +5,7 @@ JOB_GROUP="/${USER}/compute-cruchagac"
 FULLSM="$(head $1)"
 bash ./perSampleEnvs.bash $1
 LSF_DOCKER_ENV_FILE="/scratch1/fs1/cruchagac/matthewj/baseEnvs/pipelineBase.env /scratch1/fs1/cruchagac/matthewj/baseEnvs/references.env /scratch1/fs1/cruchagac/matthewj/c1in/envs/${FULLSM}.env" \
-bsub -g ${JOB_GROUP} -Is \
+bsub -g ${JOB_GROUP} -I \
 -J ngi-${USER}-test \
 -n ${THREADS} \
 -R 'select[mem>48000] rusage[mem=48GB]' \
