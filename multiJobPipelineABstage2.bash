1470fdf9b4e2f8e2ace81eb1cb4ce9af03bdc25d
Matt Johnson <matthew.j@wustl.edu>
2022-01-12 14:18:50 -0600

    job name change

--- multiJobPipelineABstage2.bash
+++ multiJobPipelineABstage2.bash
@@ -7,7 +7,7 @@ bash ./perSampleEnvs.bash $1
 for FULLSMID in $(cat $1); do
 LSF_DOCKER_ENV_FILE="/scratch1/fs1/cruchagac/matthewj/baseEnvs/pipelineBase.env /scratch1/fs1/cruchagac/matthewj/baseEnvs/references.env /scratch1/fs1/cruchagac/matthewj/c1in/envs/${FULLSMID}.env" \
 bsub -g ${JOB_GROUP} \
--J ngi-${USER}-full-$FULLSMID \
+-J ngi-${USER}-stage2-$FULLSMID \
 -n ${THREADS} \
 -R 'select[mem>48000] rusage[mem=48GB]' \
 -M ${MEM}GB \
