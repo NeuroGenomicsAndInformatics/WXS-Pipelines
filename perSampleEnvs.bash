40a21a5e50cd103d9a2c6d5ff13f7f6d98fbf566
Matt Johnson <matthew.j@wustl.edu>
2022-01-19 21:00:36 -0600

    Copy env to output directory

--- perSampleEnvs.bash
+++ perSampleEnvs.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
 ENVS_DIR="/scratch1/fs1/cruchagac/matthewj/c1in/envs"
 for FULLSMID in $(cat $1) ; do
-echo -e "FULLSMID=${FULLSMID}" > $ENVS_DIR/${FULLSMID}.env
-echo -e "INDIR=/input/${FULLSMID}" >> $ENVS_DIR/${FULLSMID}.env
+ENV_FILE="$ENVS_DIR/${FULLSMID}.env"
+echo -e "FULLSMID=${FULLSMID}" > $ENV_FILE
+echo -e "INDIR=/input/${FULLSMID}" >> $ENV_FILE
 mkdir /scratch1/fs1/cruchagac/matthewj/c1out/$FULLSMID
-echo -e "OUTDIR=/output/${FULLSMID}" >> $ENVS_DIR/${FULLSMID}.env
-echo -e "LOGFILE=/output/${FULLSMID}/log.txt" >> $ENVS_DIR/${FULLSMID}.env
-echo -e "RUN_TYPE=paddedexome" >> $ENVS_DIR/${FULLSMID}.env
+echo -e "OUTDIR=/output/${FULLSMID}" >> $ENV_FILE
+echo -e "LOGFILE=/output/${FULLSMID}/log.txt" >> $ENV_FILE
+echo -e "RUN_TYPE=paddedexome" >> $ENV_FILE
 echo -e "RGBASES="$(basename -s .rgfile /scratch1/fs1/cruchagac/matthewj/c1in/${FULLSMID}/*.rgfile)"" >> $ENVS_DIR/${FULLSMID}.env
+cp $ENV_FILE /scratch1/fs1/cruchagac/matthewj/c1out/$FULLSMID/
 done
