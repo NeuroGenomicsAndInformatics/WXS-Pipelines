f38ee1b686cbbd03cd91ed3ee728d8fbd6c49777
Matt Johnson <matthew.j@wustl.edu>
2022-01-26 10:16:56 -0600

    Output stdout text to output directory

--- perSampleEnvs.bash
+++ perSampleEnvs.bash
@@ -6,7 +6,8 @@ echo -e "FULLSMID=${FULLSMID}" > $ENV_FILE
 echo -e "INDIR=/input/${FULLSMID}" >> $ENV_FILE
 mkdir /scratch1/fs1/cruchagac/matthewj/c1out/$FULLSMID
 echo -e "OUTDIR=/output/${FULLSMID}" >> $ENV_FILE
-echo -e "LOGFILE=/output/${FULLSMID}/log.txt" >> $ENV_FILE
+echo -e "JOB_SPOOL_DIR=/output/${FULLSMID}" >> $ENV_FILE
+echo -e "LOGFILE=/output/${FULLSMID}/${FULLSMID}_runlog.txt" >> $ENV_FILE
 echo -e "RUN_TYPE=paddedexome" >> $ENV_FILE
 echo -e "RGBASES="$(basename -s .rgfile /scratch1/fs1/cruchagac/matthewj/c1in/${FULLSMID}/*.rgfile)"" >> $ENVS_DIR/${FULLSMID}.env
 cp $ENV_FILE /scratch1/fs1/cruchagac/matthewj/c1out/$FULLSMID/
