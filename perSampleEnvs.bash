6a435d1ab5f2f051188a35eec919c12f0d64fe1f
Matt Johnson <matthew.j@wustl.edu>
2022-01-27 14:00:33 -0600

    fix envs

--- perSampleEnvs.bash
+++ perSampleEnvs.bash
@@ -10,7 +10,7 @@ echo -e "OUTDIR=/output/${FULLSMID}" >> $ENV_FILE
 echo -e "LOGFILE=/output/${FULLSMID}/${FULLSMID}_runlog.txt" >> $ENV_FILE
 echo -e "RUN_TYPE=paddedexome" >> $ENV_FILE
 echo -e "RGBASES="$(basename -s .rgfile /scratch1/fs1/cruchagac/matthewj/c1in/${FULLSMID}/*.rgfile)"" >> $ENVS_DIR/${FULLSMID}.env
-cat ${BASE_ENVS_DIR}/pipelinebase.env >> $ENVS_DIR
-cat ${BASE_ENVS_DIR}/references.env >> $ENVS_DIR
+cat ${BASE_ENVS_DIR}/pipelinebase.env >> $ENV_FILE
+cat ${BASE_ENVS_DIR}/references.env >> $ENV_FILE
 cp $ENV_FILE /scratch1/fs1/cruchagac/matthewj/c1out/$FULLSMID/
 done
