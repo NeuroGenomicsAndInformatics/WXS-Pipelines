d4b1bc0102a728c95098fc8f0f353f1eac7e916e
Matt Johnson <matthew.j@wustl.edu>
2022-01-27 15:00:46 -0600

    Fix stdout and stderr locations

--- pipelineA.bash
+++ pipelineA.bash
@@ -10,8 +10,8 @@ LSF_DOCKER_ENV_FILE="/scratch1/fs1/cruchagac/matthewj/c1in/envs/${FULLSMID}.env"
 bsub -g ${JOB_GROUP} \
 -J ngi-${USER}-stage1-$FULLSMID[1-$JOBS_IN_ARRAY] \
 -n ${THREADS} \
--o /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}_s1.%J.%I.out \
--e /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}_s1.%J.err \
+-o /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}/${FULLSMID}_s1.%J.%I.out \
+-e /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}/${FULLSMID}_s1.%J.err \
 -R 'select[mem>102000] rusage[mem=100000] span[hosts=1]' \
 -M 120000000 \
 -G compute-cruchagac \
@@ -22,8 +22,8 @@ bsub -g ${JOB_GROUP} \
 -w "done(\"ngi-${USER}-stage1-$FULLSMID\")" \
 -J ngi-${USER}-stage2-$FULLSMID \
 -n ${THREADS} \
--o /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}_s2.%J.out \
--e /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}_s2.%J.err \
+-o /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}/${FULLSMID}_s2.%J.out \
+-e /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}/${FULLSMID}_s2.%J.err \
 -R 'select[mem>102000] rusage[mem=100000] span[hosts=1]' \
 -M 120000000 \
 -G compute-cruchagac \
