117b0b5f4a6c3a2bd778b936290c231c8c32a325
Matt Johnson <matthew.j@wustl.edu>
2022-01-28 14:53:13 -0600

    pipe C test script thread change

--- pipelineC.bash
+++ pipelineC.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Check if running in the background. If not, call itself in the background and exit the foreground version
-export THREADS=1
+export THREADS=8
 export MEM=32
 JOB_GROUP="/${USER}/compute-cruchagac"
 bgadd -L 10 ${JOB_GROUP}
