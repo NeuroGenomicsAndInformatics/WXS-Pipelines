95319e23472fbfc87659f6726e4f2c5f1e25b269
Matt Johnson <matthew.j@wustl.edu>
2022-01-31 12:55:47 -0600

    Transfer output files script

---
+++ transferOutputFiles.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+OUTDIR=/scratch1/fs1/cruchagac/matthewj/c1out
+FINAL_OUTDIR=/storage1/fs1/cruchagac/Active/matthewj/c1out
+for FULLSMID in $(cat $1); do
+mkdir ${FINAL_OUTDIR}/${FULLSMID}
+rsync ${OUTDIR}/${FULLSMID}/*.env ${FINAL_OUTDIR}/${FULLSMID}
+rsync ${OUTDIR}/${FULLSMID}/*.cram ${FINAL_OUTDIR}/${FULLSMID}
+rsync ${OUTDIR}/${FULLSMID}/*_GATK* ${FINAL_OUTDIR}/${FULLSMID}
+rsync ${OUTDIR}/${FULLSMID}/*vcf* ${FINAL_OUTDIR}/${FULLSMID}
+rsync ${OUTDIR}/${FULLSMID}/*verifybam.selfSM ${FINAL_OUTDIR}/${FULLSMID}
+rsync ${OUTDIR}/${FULLSMID}/*_exome_* ${FINAL_OUTDIR}/${FULLSMID}
+done
