51be0113d537cbebec2e984c321cb76a9add32dd
Matt Johnson <matthew.j@wustl.edu>
2022-01-31 14:09:18 -0600

    Fixed staging issue

--- fileTransferMakeRgfiles.bash
+++ fileTransferMakeRgfiles.bash
@@ -12,12 +12,12 @@ PROJECT="$2"
 PROJECTNAME="$3"
 FULLSM=$(echo ${SM}.${PROJECTNAME}\^${BARCODE}\^${PROJECT})
 RGBASE="${FULLSM}.${FLOWCELL}^${LANE}"
-RGFILE="./RGFILES/${RGBASE}.rgfile"
-touch $RGFILE
-echo "@RG\tID:${FLOWCELL}:${LANE}\tPL:illumina\tPU:${FLOWCELL}:${LANE}:${BARCODE}\tLB:${BARCODE}\tSM:${SM}.${PROJECTNAME}\tDS:${FULLSM}" > $RGFILE
 STAGE_DIR="/scratch1/fs1/cruchagac/matthewj/c1in/${FULLSM}"
 mkdir ${STAGE_DIR}
-rsync ${RGFILE} ${STAGE_DIR}/${RGBASE}.rgfile
+RGFILE="$STAGE_DIR/${RGBASE}.rgfile"
+touch $RGFILE
+echo "@RG\tID:${FLOWCELL}:${LANE}\tPL:illumina\tPU:${FLOWCELL}:${LANE}:${BARCODE}\tLB:${BARCODE}\tSM:${SM}.${PROJECTNAME}\tDS:${FULLSM}" > $RGFILE
+#rsync ${RGFILE} ${STAGE_DIR}/${RGBASE}.rgfile
 FQ1FILE="$(echo $FILE | cut -d "," -f 1)"
 rsync ${FILES_DIR}/${FQ1FILE} ${STAGE_DIR}/${RGBASE}.r1.fq.gz
 rsync ${FILES_DIR}/${FQ1FILE%_*}_R2.fastq.gz ${STAGE_DIR}/${RGBASE}.r2.fq.gz
