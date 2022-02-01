f7d44de6a751566e878e6403e6e02d2f7a2ea866
Matt Johnson <matthew.j@wustl.edu>
2022-02-01 10:07:57 -0600

    Speedup with 16 THREADS

--- pipelineA.bash
+++ pipelineA.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-export THREADS=12
+export THREADS=16
 export MEM=96
 JOB_GROUP="/${USER}/compute-cruchagac"
 bgadd -L 10 ${JOB_GROUP}
@@ -12,7 +12,7 @@ bsub -g ${JOB_GROUP} \
 -n ${THREADS} \
 -o /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}/${FULLSMID}_s1.%J.%I.out \
 -e /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}/${FULLSMID}_s1.%J.%I.err \
--R 'select[mem>102000 && tmp>10] rusage[mem=100000, tmp=10] span[hosts=1]' \
+-R 'select[mem>102000 && tmp>10] rusage[mem=100000, tmp=10]' \
 -M 120000000 \
 -G compute-cruchagac \
 -q general \
