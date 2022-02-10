ebbf089eac2e544773f50a48753e29c23144f686
Matt Johnson <matthew.j@wustl.edu>
2022-02-10 11:24:07 -0600

    separate job group for dev

--- dev_wxspipeline.bash
+++ dev_wxspipeline.bash
@@ -5,7 +5,7 @@ export LSF_DOCKER_VOLUMES="/scratch1/fs1/cruchagac/matthewj/c1in:/input \
 /storage1/fs1/cruchagac/Active/matthewj/c1out:/final_output"
 export THREADS=16
 export MEM=96
-JOB_GROUP="/${USER}/compute-cruchagac"
+JOB_GROUP="/${USER}-dev/compute-cruchagac"
 bgadd -L 10 ${JOB_GROUP}
 bash ./perSampleEnvs.bash $1
 for FULLSMID in $(cat $1); do
