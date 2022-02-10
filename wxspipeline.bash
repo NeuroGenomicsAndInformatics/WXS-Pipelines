bbc97f50c578c8e83d8841d61f4c6f5594da6cc1
Matt Johnson <matthew.j@wustl.edu>
2022-02-10 14:19:48 -0600

    put workfile back

--- wxspipeline.bash
+++ wxspipeline.bash
@@ -8,7 +8,7 @@ export THREADS=16
 export MEM=96
 JOB_GROUP="/${USER}/compute-cruchagac"
 bgadd -L 10 ${JOB_GROUP}
-FULLSMID="$1"
+for FULLSMID in $(cat $1); do
 bash ./makeSampleEnv.bash $FULLSMID
 JOBS_IN_ARRAY=$(ls /scratch1/fs1/cruchagac/matthewj/c1in/${FULLSMID}/*.rgfile | wc -w)
 LSF_DOCKER_ENV_FILE="/scratch1/fs1/cruchagac/matthewj/c1in/envs/${FULLSMID}.env" \
@@ -35,3 +35,4 @@ bsub -g ${JOB_GROUP} \
 -G compute-cruchagac \
 -q general \
 -a 'docker(mjohnsonngi/wxspipeline:latest)' /scripts/pipelineStage2.bash
+done
