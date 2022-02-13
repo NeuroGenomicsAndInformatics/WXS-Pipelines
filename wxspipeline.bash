ea3a20ce1ca427d7a0759307d5dce987ac669fed
Matt Johnson <matthew.j@wustl.edu>
2022-02-13 14:08:25 -0600

    Removed output file to combine with err and send to email

--- wxspipeline.bash
+++ wxspipeline.bash
@@ -15,7 +15,6 @@ LSF_DOCKER_ENV_FILE="/scratch1/fs1/cruchagac/matthewj/c1in/envs/${FULLSMID}.env"
 bsub -g ${JOB_GROUP} \
 -J ngi-${USER}-stage1-$FULLSMID[1-$JOBS_IN_ARRAY] \
 -n ${THREADS} \
--o /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}/${FULLSMID}_s1.%J.%I.out \
 -e /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}/${FULLSMID}_s1.%J.%I.err \
 -R 'select[mem>102000] rusage[mem=100000]' \
 -M 120000000 \
@@ -28,7 +27,6 @@ bsub -g ${JOB_GROUP} \
 -J ngi-${USER}-stage2-$FULLSMID \
 -n ${THREADS} \
 -N \
--o /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}/${FULLSMID}_s2.%J.out \
 -e /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}/${FULLSMID}_s2.%J.err \
 -R 'select[mem>102000] rusage[mem=100000] span[hosts=1]' \
 -M 120000000 \
