320be0cf7373bb9fd9593be4d5c146fa1fd167fe
Matt Johnson <matthew.j@wustl.edu>
2022-02-15 14:05:09 -0600

    span

--- wxspipeline.bash
+++ wxspipeline.bash
@@ -16,7 +16,7 @@ bsub -g ${JOB_GROUP} \
 -J ngi-${USER}-stage1-$FULLSMID[1-$JOBS_IN_ARRAY] \
 -n ${THREADS} \
 -o /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}/${FULLSMID}_s1.%J.%I.out \
--R 'select[mem>102000] rusage[mem=100000] span[hosts=1]' \
+-R 'select[mem>102000] rusage[mem=100000]' \
 -M 120000000 \
 -G compute-cruchagac \
 -q general \
