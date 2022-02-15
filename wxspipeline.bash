3ecf2960282968ca20ef990cb3729b5f31373d56
Matt Johnson <matthew.j@wustl.edu>
2022-02-15 16:41:49 -0600

    threads

--- wxspipeline.bash
+++ wxspipeline.bash
@@ -4,7 +4,7 @@ export LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active/matthewj/c1in:/staged_
 /scratch1/fs1/cruchagac/matthewj/ref:/ref \
 /scratch1/fs1/cruchagac/matthewj/c1out:/output \
 /storage1/fs1/cruchagac/Active/matthewj/c1out:/final_output"
-export THREADS=32
+export THREADS=16
 export MEM=352
 JOB_GROUP="/${USER}/compute-cruchagac"
 bgadd -L 10 ${JOB_GROUP}
@@ -28,8 +28,8 @@ bsub -g ${JOB_GROUP} \
 -n ${THREADS} \
 -N \
 -o /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}/${FULLSMID}_s2.%J.out \
--R 'select[mem>102000] rusage[mem=100000] span[hosts=1]' \
--M 120000 \
+-R 'select[mem>372000] rusage[mem=366000]' \
+-M 380000 \
 -G compute-cruchagac \
 -q general \
 -a 'docker(mjohnsonngi/wxspipeline:latest)' /scripts/pipelineStage2.bash
