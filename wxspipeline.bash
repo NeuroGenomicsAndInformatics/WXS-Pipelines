efbd8cd9d261c2477cd50fe17c5f3b95fde0719a
Matt Johnson c1 <matthew.j@wustl.edu>
2022-02-19 14:50:27 -0600

    working runner

--- wxspipeline.bash
+++ wxspipeline.bash
@@ -5,7 +5,7 @@ export LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active/matthewj/c1in:/staged_
 /scratch1/fs1/cruchagac/matthewj/c1out:/output \
 /storage1/fs1/cruchagac/Active/matthewj/c1out:/final_output"
 export THREADS=32
-export MEM=352
+export MEM=256
 JOB_GROUP="/${USER}/compute-cruchagac"
 bgadd -L 10 ${JOB_GROUP}
 for FULLSMID in $(cat $1); do
@@ -14,15 +14,14 @@ JOBS_IN_ARRAY=$(ls /storage1/fs1/cruchagac/Active/matthewj/c1in/${FULLSMID}/*.rg
 LSF_DOCKER_ENV_FILE="/scratch1/fs1/cruchagac/matthewj/c1in/envs/${FULLSMID}.env" \
 bsub -g ${JOB_GROUP} \
 -J ngi-${USER}-stage1-$FULLSMID[1-$JOBS_IN_ARRAY] \
--n 1 \
+-n ${THREADS} \
+-r \
 -o /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}/${FULLSMID}_s1.%J.%I.out \
--R 'select[mem>365000] rusage[mem=365000] ' \
--R "affinity[thread(32):membind=localprefer]" \
--m compute1-exec-285 \
--M 380000 \
+-R 'select[mem>275000] rusage[mem=270000]' \
+-M 275000 \
 -G compute-cruchagac \
 -q general \
--a 'docker(mjohnsonngi/wxspipeline:latest)' /scripts/pipelineStage1.bash && \
+-a 'docker(mjohnsonngi/wxspipeline:dev)' /scripts/pipelineStage1.bash && \
 LSF_DOCKER_ENV_FILE="/scratch1/fs1/cruchagac/matthewj/c1in/envs/${FULLSMID}.env" \
 bsub -g ${JOB_GROUP} \
 -w "done(\"ngi-${USER}-stage1-$FULLSMID\")" \
@@ -30,9 +29,9 @@ bsub -g ${JOB_GROUP} \
 -n ${THREADS} \
 -N \
 -o /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}/${FULLSMID}_s2.%J.out \
--R 'select[mem>372000] rusage[mem=366000]' \
--M 380000 \
+-R 'select[mem>275000] rusage[mem=270000]' \
+-M 275000 \
 -G compute-cruchagac \
 -q general \
--a 'docker(mjohnsonngi/wxspipeline:latest)' /scripts/pipelineStage2.bash
+-a 'docker(mjohnsonngi/wxspipeline:dev)' /scripts/pipelineStage2.bash
 done
