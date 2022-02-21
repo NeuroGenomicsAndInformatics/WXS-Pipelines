929482415cede2d3c85821518cd22abb81f66bdf
Matt Johnson <matthew.j@wustl.edu>
2022-02-21 16:04:03 -0600

    pipeline requirements

--- wxspipeline.bash
+++ wxspipeline.bash
@@ -4,8 +4,8 @@ export LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active/matthewj/c1in:/staged_
 /scratch1/fs1/cruchagac/matthewj/ref:/ref \
 /scratch1/fs1/cruchagac/matthewj/c1out:/output \
 /storage1/fs1/cruchagac/Active/matthewj/c1out:/final_output"
-export THREADS=32
-export MEM=256
+export THREADS=16
+export MEM=144
 JOB_GROUP="/${USER}/compute-cruchagac"
 bgadd -L 10 ${JOB_GROUP}
 for FULLSMID in $(cat $1); do
@@ -14,11 +14,11 @@ JOBS_IN_ARRAY=$(ls /storage1/fs1/cruchagac/Active/matthewj/c1in/${FULLSMID}/*.rg
 LSF_DOCKER_ENV_FILE="/scratch1/fs1/cruchagac/matthewj/c1in/envs/${FULLSMID}.env" \
 bsub -g ${JOB_GROUP} \
 -J ngi-${USER}-stage1-$FULLSMID[1-$JOBS_IN_ARRAY] \
--n ${THREADS} \
+-n 16 \
 -r \
 -o /scratch1/fs1/cruchagac/matthewj/c1out/${FULLSMID}/${FULLSMID}_s1.%J.%I.out \
--R 'select[mem>275000] rusage[mem=270000] span[hosts=1]' \
--M 275000 \
+-R 'select[mem>150000] rusage[mem=150000/job] span[hosts=1]' \
+-M 160000 \
 -G compute-cruchagac \
 -q general \
 -a 'docker(mjohnsonngi/wxspipeline:dev)' /scripts/pipelineStage1.bash && \
