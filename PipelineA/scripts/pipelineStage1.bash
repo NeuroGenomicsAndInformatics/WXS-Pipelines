e06bb88c2f13ca44f1dc530614e931b23507deef
Matt Johnson <matthew.j@wustl.edu>
2022-02-22 16:28:39 -0600

    Errant parenthesis

--- PipelineA/scripts/pipelineStage1.bash
+++ PipelineA/scripts/pipelineStage1.bash
@@ -2,7 +2,8 @@
 source /scripts/pipelineCommonFunctions.bash
 source /scripts/pipelineStage1HelperFunctions.bash
 SAMPLEID=$(echo $FULLSMID | cut -d '^' -f 1)
-RGBASE="$(echo ${RGBASES} | cut -d ' ' -f $LSB_JOBINDEX)"
+RG_ARRAY=($RGBASES)
+RGBASE="${RG_ARRAY[$LSB_JOBINDEX-1]}"
 MEM_SPLIT=$((${S1MEM}/${S1THREADS}))
 reportToLog "Starting pipeline for $RGBASE. Staging Data to scratch1."
 stageDataForRGBASE
@@ -10,7 +11,7 @@ FQ1="$(find $INDIR -name "${RGBASE}*1*" -print)"
 FQ1EXT="$(echo ${FQ1##*${RGBASE}})"
 if [[ -e $FQ1 ]]; then FQ2="$(echo $INDIR/${RGBASE}${FQ1EXT/1/2})"; else unset FQ2; fi
 FQI="$(find $INDIR -name "${RGBASE}*f*q*" -print)"
-echo -n "" > ${OUTDIR}/stage1complete.txt
+touch ${OUTDIR}/stage1complete.txt
 reportToLog "Aligning and sorting"
 if [[ -e $FQ1 ]] && [[ -e $FQ2 ]]; then
 if [[ $(wc -c $FQ1) < 14000000000 ]]; then alignSortPairedFQs || alignSortPairedHugeFQs; else alignSortPairedHugeFQs; fi
@@ -34,6 +35,6 @@ validateCurrentBam
 reportToLog "Validated. Adding to stage1complete list."
 #NOTE: This can lead to a race condition on the list. A failed Stage 2 could be caused by the stage1complete.txt file being messed up. Fixing the file manually solves this and Stage 2 can be run normally after.
 saveToOutputDirectory ${CURRENT_BAM}
-if [[ $(wc -c $CURRENT_BAM) < 80000000 ]]; then echo -e "${CURRENT_BAM##*/}" >> ${OUTDIR}/stage1complete.txt; else cleanup; exit 5; fi
+if [[ $(wc -c $CURRENT_BAM) > 80000000 ]]; then echo -e "${CURRENT_BAM##*/}" >> ${OUTDIR}/stage1complete.txt; else cleanup; exit 5; fi
 cleanUp && rm ${STAGE_INDIR}/${RGBASE}*
 reportToLog "Finished for $RGBASE"
