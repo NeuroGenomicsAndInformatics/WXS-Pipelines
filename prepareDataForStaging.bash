1c45db7f76433aa67f0e4a450ae5fa06c7d9cfab
Matt Johnson <matthew.j@wustl.edu>
2022-03-03 16:22:57 -0600

    Fix naming convention

--- prepareDataForStaging.bash
+++ prepareDataForStaging.bash
@@ -1,7 +1,6 @@
 #!bin/bash
 SAMP_MAP="$1"
 PROJECT="$2"
-PROJECTNAME="$3"
 WORKFILE="/storage1/fs1/cruchagac/Active/${USER}/workfile$3.txt"
 touch $WORKFILE
 METAFILE="/storage1/fs1/cruchagac/Active/${USER}/$3-FULLSM-SM-DNA-PR-RGBASE.csv"
@@ -14,16 +13,16 @@ FLOWCELL=$(echo $FILE | cut -d "," -f 2)
 LANE=$(echo $FILE | cut -d "," -f 4)
 SM=$(echo $(echo $FILE | cut -d "," -f 6) | cut -d "-" -f 2)
 BARCODE=$(echo $(echo $FILE | cut -d "," -f 6) | cut -d "-" -f 3)
-FULLSM=$(echo ${SM}.${PROJECTNAME}\^${BARCODE}\^${PROJECT})
+FULLSM=$(echo ${SM}\^${BARCODE}\^${PROJECT})
 RGBASE="${FULLSM}.${FLOWCELL}^${LANE}"
 STAGE_DIR="/storage1/fs1/cruchagac/Active/${USER}/c1in/${FULLSM}"
 if [ ! -e $STAGE_DIR ]; then mkdir ${STAGE_DIR}; echo $FULLSM >> $WORKFILE; fi
 RGFILE="$STAGE_DIR/${RGBASE}.rgfile"
 touch $RGFILE
-echo "@RG\tID:${FLOWCELL}:${LANE}\tPL:illumina\tPU:${FLOWCELL}:${LANE}:${BARCODE}\tLB:${BARCODE}\tSM:${SM}.${PROJECTNAME}\tDS:${FULLSM}" > $RGFILE
+echo "@RG\tID:${FLOWCELL}:${LANE}\tPL:illumina\tPU:${FLOWCELL}:${LANE}:${BARCODE}\tLB:${BARCODE}\tSM:${SM}\tDS:${FULLSM}" > $RGFILE
 FQ1FILE="$(echo $FILE | cut -d "," -f 1)"
 ln -s ${FILES_DIR}/${FQ1FILE} ${STAGE_DIR}/${RGBASE}.r1.fq.gz
 ln -s ${FILES_DIR}/${FQ1FILE%_*}_R2.fastq.gz ${STAGE_DIR}/${RGBASE}.r2.fq.gz
-echo -e "$FULLSM,$SM.$PROJECTNAME,$BARCODE,$PROJECT,$RGBASE,${FQ1FILE%_*},$(wc -c ${FILES_DIR}/$FQ1FILE | cut -d' ' -f1)" >> $METAFILE
+echo -e "$FULLSM,$SM,$BARCODE,$PROJECT,$RGBASE,${FQ1FILE%_*},$(wc -c ${FILES_DIR}/$FQ1FILE | cut -d' ' -f1)" >> $METAFILE
 fi
 done
