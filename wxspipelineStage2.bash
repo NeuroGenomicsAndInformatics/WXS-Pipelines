d491453e075cf6373b3ed6819253ad1be9e2d8f7
Matt Johnson <matthew.j@wustl.edu>
2022-03-14 14:59:42 -0500

    stupid mistake

--- wxspipelineStage2.bash
+++ wxspipelineStage2.bash
@@ -9,6 +9,7 @@ bgadd -L 10 ${JOB_GROUP}
 for FULLSMID in $(cat $1); do
   bash ./makeSampleEnv.bash $FULLSMID
   LSF_DOCKER_ENV_FILE="/scratch1/fs1/cruchagac/${USER}/c1in/envs/${FULLSMID}.env" \
+  bsub -g ${JOB_GROUP} \
   -J ngi-${USER}-stage2-${FULLSMID} \
   -N \
   -o /scratch1/fs1/cruchagac/${USER}/c1out/logs/${FULLSMID}/${FULLSMID}_s2.%J.out \
