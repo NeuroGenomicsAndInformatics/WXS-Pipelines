4f13346ca6b5d2bf7d391c4ed3e32a4dab6fb14c
Matt Johnson c1 <matthew.j@wustl.edu>
2022-03-23 10:43:16 -0500

    gpu wrapper update

--- wxspipelinegpu.bash
+++ wxspipelinegpu.bash
@@ -14,11 +14,10 @@ LSF_DOCKER_ENTRYPOINT=/bin/sh \
 TMP_DIR="/scratch1/fs1/cruchagac/parabricks-tmp" \
 bsub -g ${JOB_GROUP} \
 -J ngi-${USER}-stage1gpu-%J \
--n 32 \
 -o /scratch1/fs1/cruchagac/${USER}/c1out/logs/GPU.%J.out \
--R 'gpuhost rusage[mem=196GB] span[hosts=1]' \
--M 196GB \
+-R 'select[gpuhost && mem>64GB && ncpus>15] rusage[mem=64GB] span[hosts=1]' \
+-M 64GB \
 -G compute-cruchagac \
 -q general \
--gpu "num=4:gmodel=TeslaV100_SXM2_32GB:j_exclusive=yes" \
+-gpu "num=1:gmodel=TeslaV100_SXM2_32GB:j_exclusive=yes" \
 -a 'docker(gcr.io/ris-registry-shared/parabricks)' pbrun "$@"
