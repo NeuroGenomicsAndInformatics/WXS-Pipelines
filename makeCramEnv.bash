e9dec21be2b35dcc23f23efc7c30b2b923042796
Matt Johnson <matthew.j@wustl.edu>
2022-04-04 10:20:45 -0500

    parsing issues

--- makeCramEnv.bash
+++ makeCramEnv.bash
@@ -2,14 +2,14 @@
 ENVS_DIR="/scratch1/fs1/cruchagac/${USER}/c1in/envs"
 BASE_ENVS_DIR="./baseEnvs"
 LINE="$1"
-FULLSMID=$(echo $LINE | cut -d ' ' -f1)
-export CRAM=$(echo $LINE | cut -d ' ' -f2)
+FULLSMID="$(echo $LINE | cut -d ' ' -f1)"
+export CRAM="$(echo $LINE | cut -d ' ' -f2)"
 STAGE1_WORKFILE="~/work${CRAM##/*}.txt"
 ENV_FILE="$ENVS_DIR/${CRAM}.env"
 echo -e "FULLSMID=${FULLSMID}" > $ENV_FILE
 echo -e "STAGE_INDIR=/storage1/fs1/cruchagac/Active/${USER}/c1in" >> $ENV_FILE
 echo -e "INDIR=/input/${FULLSMID}" >> $ENV_FILE
-mkdir /scratch1/fs1/cruchagac/${USER}/c1out/$FULLSMID
+[ ! -d /scratch1/fs1/cruchagac/$USER/c1out/${FULLSMID} ] && mkdir /scratch1/fs1/cruchagac/${USER}/c1out/$FULLSMID
 echo -e "OUTDIR=/output/${FULLSMID}" >> $ENV_FILE
 echo -e "STAGE1_WORKFILE=${STAGE1_WORKFILE}" >> $ENV_FILE
 cat ${BASE_ENVS_DIR}/pipelinebase.env >> $ENV_FILE
