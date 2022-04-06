05803b64683979afda161d906ad838a947c3a1ae
Matt Johnson <matthew.j@wustl.edu>
2022-04-06 10:38:36 -0500

    allow running from anywhere and move LSF_DOCKER_VOLUMES

--- testpipelineC.bash
+++ testpipelineC.bash
@@ -1,20 +1,22 @@
 #!/bin/bash
-export LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active:/storage1/fs1/cruchagac/Active \
-/scratch1/fs1/cruchagac/${USER}/c1in:/input \
-/scratch1/fs1/cruchagac/WXSref:/ref \
-/scratch1/fs1/cruchagac/${USER}/c1out:/output \
-/storage1/fs1/cruchagac/Active/${USER}/c1out:/final_output"
 export THREADS=8
 export MEM=32
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 JOB_GROUP="/${USER}/compute-cruchagac"
 bgadd -L 10 ${JOB_GROUP}
 cat $1 | while read LINE; do
 CRAM="$(echo $LINE | cut -d ' ' -f2)"
-bash ./makeCramEnv.bash $LINE
+bash ${SCRIPT_DIR}/makeCramEnv.bash $LINE
+LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active:/storage1/fs1/cruchagac/Active \
+/scratch1/fs1/cruchagac/${USER}/c1in:/input \
+/scratch1/fs1/cruchagac/WXSref:/ref \
+/scratch1/fs1/cruchagac/${USER}/c1out:/output \
+/storage1/fs1/cruchagac/Active/${USER}/c1out:/final_output" \
 LSF_DOCKER_ENV_FILE="/scratch1/fs1/cruchagac/${USER}/c1in/envs/${CRAM}.env" \
 bsub -g ${JOB_GROUP} \
 -J ngi-${USER}-stage0-$CRAM \
 -n ${THREADS} \
+-cwd ${SCRIPT_DIR} \
 -o /scratch1/fs1/cruchagac/${USER}/c1out/${CRAM%.cram}/${CRAM%.cram}.%J.out \
 -R 'select[mem>32000] rusage[mem=32000]' \
 -M 46000000 \
