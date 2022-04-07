9f178c5557c610b5a545d40f2f2ef497eb9a4828
Matt Johnson <matthew.j@wustl.edu>
2022-04-07 10:13:17 -0500

    fixes for stage 2

--- wxspipelineStage2.bash
+++ wxspipelineStage2.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
+export LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active:/storage1/fs1/cruchagac/Active \
+  /scratch1/fs1/cruchagac/${USER}/c1in:/input \
+  /scratch1/fs1/cruchagac/WXSref:/ref \
+  /scratch1/fs1/cruchagac/${USER}/c1out:/output \
+  /storage1/fs1/cruchagac/Active/${USER}/c1out:/final_output"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 JOB_GROUP="/${USER}/compute-cruchagac"
 bgadd -L 10 ${JOB_GROUP}
 for FULLSMID in $(cat $1); do
   bash ${SCRIPT_DIR}/makeSampleEnv.bash $FULLSMID
-  LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active:/storage1/fs1/cruchagac/Active \
-  /scratch1/fs1/cruchagac/${USER}/c1in:/input \
-  /scratch1/fs1/cruchagac/WXSref:/ref \
-  /scratch1/fs1/cruchagac/${USER}/c1out:/output \
-  /storage1/fs1/cruchagac/Active/${USER}/c1out:/final_output" \
   LSF_DOCKER_ENV_FILE="/scratch1/fs1/cruchagac/${USER}/c1in/envs/${FULLSMID}.env" \
   bsub -g ${JOB_GROUP} \
   -J ngi-${USER}-stage2-${FULLSMID} \
