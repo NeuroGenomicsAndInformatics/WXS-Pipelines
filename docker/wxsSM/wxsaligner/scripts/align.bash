a76d778283969bcb3d7f52768fbd0f74d284fbea
Matt Johnson <matthew.j@wustl.edu>
2022-09-26 12:08:31 -0500

    Working GPU alignment docker

--- docker/wxsSM/wxsaligner/scripts/align.bash
+++ docker/wxsSM/wxsaligner/scripts/align.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [[ -z $CUDA_VISIBLE_DEVICES ]]; then exit 66
-else bash /scripts/gpualign.bash $1
+else bash /scripts/gpualign.bash
 fi
--- docker/wxsSM/wxsaligner/scripts/gpualign.bash
+++ docker/wxsSM/wxsaligner/scripts/gpualign.bash
@@ -1,13 +1,12 @@
 #!/bin/bash
-export NVIDIA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
-for VAR in $(cat /scratch1/fs1/cruchagac/$USER/c1in/envs/${1}.env); do
-export $VAR;
+for VAR in $(printenv | grep CUDA_VISIBLE_DEVICES); do
+export ${VAR/CUDA/NVIDIA}
 done
 bash /scripts/stageinfqs.bash ${FULLSMID}
 pbrun fq2bam \
     --ref ${REF_FASTA} \
     --in-fq-list ${INDIR}/infqfile.txt \
-    --out-bam ${INDIR}/${BAM} \
+    --out-bam ${OUTDIR}/${BAM} \
     --out-duplicate-metrics ${METDIR}/${FULLSMID}.dup.metrics.txt \
     --num-gpus 1 \
     --tmp-dir ${TMP_DIR} \
