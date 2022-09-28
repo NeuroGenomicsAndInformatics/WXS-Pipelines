aa2c710631d199203337e9c5f71133aea60bf74c
Matt Johnson <matthew.j@wustl.edu>
2022-09-27 21:16:18 -0500

    Freemix fix

--- docker/wxsSM/wxsfreemix/scripts/vbid.bash
+++ docker/wxsSM/wxsfreemix/scripts/vbid.bash
@@ -4,4 +4,5 @@ VerifyBamID \
   --SVDPrefix /VerifyBamID/resource/1000g.phase3.100k.b38.vcf.gz.dat \
   --Reference ${REF_FASTA} \
   --NumThread $LSB_MAX_NUM_PROCESSORS \
+  --Output $1.vbid2 \
   --max-depth 1000 \
