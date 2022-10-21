3ac825a0c6beddab3df4fc86c60843c10c290921
Matt Johnson <matthew.j@wustl.edu>
2022-10-21 10:07:28 -0500

    2 gpu align with debug flags

---
+++ docker/wxsSM/wxsaligner/scripts/gpualign2.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+for VAR in $(printenv | grep CUDA_VISIBLE_DEVICES); do
+export ${VAR/CUDA/NVIDIA}
+done
+bash /scripts/stageinfqs.bash ${FULLSMID}
+pbrun fq2bam \
+    --ref ${REF_FASTA} \
+    --in-fq-list ${INDIR}/infqfile.txt \
+    --out-bam ${INDIR}/${BAM} \
+    --out-duplicate-metrics ${METDIR}/${FULLSMID}.dup.metrics.txt \
+    --num-gpus 2 \
+    --tmp-dir ${TMP_DIR} \
+    --x3 \
+    --x4 \
+    || exit 66
+samtools view -@ $LSB_MAX_NUM_PROCESSORS -C -T ${REF_FASTA} -o $OUTDIR/$CRAM $INDIR/$BAM
+samtools index -@ $LSB_MAX_NUM_PROCESSORS $OUTDIR/$CRAM && rm -R $INDIR
