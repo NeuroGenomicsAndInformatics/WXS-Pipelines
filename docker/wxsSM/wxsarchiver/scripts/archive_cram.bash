0d7dd4934db0f9812634097fb8b7fae3b9bc2e08
Matt Johnson <matthew.j@wustl.edu>
2022-11-02 11:17:10 -0500

    Change to allow archiving to different location

--- docker/wxsSM/wxsarchiver/scripts/archive_cram.bash
+++ docker/wxsSM/wxsarchiver/scripts/archive_cram.bash
@@ -1,2 +1,6 @@
 #!/bin/bash
-samtools view -C -T $REF_FASTA -@ $(( LSB_MAX_NUM_PROCESSORS -1 )) --output-fmt-option archive --output-fmt-option embed_ref -o ${1%.cram}.arc_er.cram $1
+OUTDIR=$2
+[ -z $2 ] && OUTDIR=${1%/*}
+INFILE=${1##*/}
+OUTPUT=${OUTDIR}/${INFILE/%.cram/.arc_er.cram}
+samtools view -C -T $REF_FASTA -@ $(( LSB_MAX_NUM_PROCESSORS -1 )) --output-fmt-option archive --output-fmt-option embed_ref -o $OUTPUT $1
