1db237704686d62109553a128cc33a886873d4ae
Matt Johnson <matthew.j@wustl.edu>
2022-12-09 11:03:46 -0600

    Freemix for exomes

---
+++ wrappers/freemix_exome_wrapper copy.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+FULL_CRAM=$1
+
+REF_DIR="/scratch1/fs1/fernandezv/WXSref"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+JOB_GROUP_QC="/${USER}/compute-fernandezv/qc"
+
+LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active:/storage1/fs1/cruchagac/Active \
+${REF_DIR}:/ref" \
+LSF_DOCKER_ENV_FILE=$SCRIPT_DIR/../baseEnvs/references_2_0.env \
+bsub -g ${JOB_GROUP_QC} \
+    -J ngi-${USER}-freemix \
+    -Ne \
+    -n 2 \
+    -R 'rusage[mem=20GB,tmp=2GB]' \
+    -G compute-fernandezv \
+    -q general \
+    -a 'docker(mjohnsonngi/wxsfreemix:2.0)' \
+    bash /scripts/vbid_exome.bash $FULL_CRAM
