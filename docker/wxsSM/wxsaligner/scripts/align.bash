5c20a357700be19995c8302563d770f60a840604
Matt Johnson <matthew.j@wustl.edu>
2022-12-20 11:22:57 -0600

    Mistake in input selection logic fixed

--- docker/wxsSM/wxsaligner/scripts/align.bash
+++ docker/wxsSM/wxsaligner/scripts/align.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [[ -z $(find $STAGE_INDIR -name "*.cram") ]]; then
+if [[ -n $(find $STAGE_INDIR -name "*.cram") ]]; then
   bash /scripts/align_cram.bash "$2"
-elif [[ -z $(find $STAGE_INDIR -name "*.bam") ]]; then
+elif [[ -n $(find $STAGE_INDIR -name "*.bam") ]]; then
   bash /scripts/align_bam.bash
 else
   bash /scripts/align_fqs.bash
