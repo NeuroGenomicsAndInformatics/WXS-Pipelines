2c212dcd75621f7a1d2da3152161ad8543c7e6fb
Matt Johnson <matthew.j@wustl.edu>
2023-02-02 09:39:18 -0600

    cleanup unused files

--- docker/wxsSM/wxsaligner/scripts/align_cramspark.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export UNWRAP_FASTA="$1"
-if [[ -z $CUDA_VISIBLE_DEVICES ]]; then
-  bash /scripts/stageincram_spark.bash $UNWRAP_FASTA \
-  && bash /scripts/cpualign_spark.bash
-else
-  bash /scripts/stageincram_gpu.bash $UNWRAP_FASTA \
-  && bash /scripts/gpualign.bash
-fi
--- docker/wxsSM/wxsaligner/scripts/align_spark.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-bash /scripts/stageinfqs.bash
-if [[ -z $CUDA_VISIBLE_DEVICES ]]; then
-  bash /scripts/cpualign_spark.bash
-else
-  bash /scripts/gpualign.bash
-fi
