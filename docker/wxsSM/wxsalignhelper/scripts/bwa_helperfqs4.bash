795bfb4e72e6cc60038f5a4c4495141237fa921b
Matt Johnson <matthew.j@wustl.edu>
2023-02-09 11:53:28 -0600

    Better finding RGs

--- docker/wxsSM/wxsalignhelper/scripts/bwa_helperfqs4.bash
+++ docker/wxsSM/wxsalignhelper/scripts/bwa_helperfqs4.bash
@@ -2,10 +2,10 @@
 THREADS=$(( LSB_MAX_NUM_PROCESSORS * 2 ))
 FQ1=$(head -n $LSB_JOBINDEX ${INDIR}/infqfile.txt | tail -n1 | cut -d ' ' -f1)
 echo $FQ1
-RGFILE="${OUTDIR}/${FULLSMID}.$(echo ${FQ1##*/} | cut -d_ -f1 | cut -d. -f1)_$(echo ${FQ1##*/} | cut -d_ -f1 | cut -d. -f2).rgfile"
+#RGFILE="${OUTDIR}/${FULLSMID}.$(echo ${FQ1##*/} | cut -d_ -f1 | cut -d. -f1)_$(echo ${FQ1##*/} | cut -d_ -f1 | cut -d. -f2).rgfile"
 if [[ $(wc -c $FQ1 | cut -d' ' -f1) -lt 7000000000 ]]; then
 bwa-mem2 mem -M -t $THREADS -K 10000000 \
-  -R $(head -n1 ${RGFILE}) \
+  -R $(head -n $LSB_JOBINDEX ${INDIR}/infqfile.txt | tail -n1 | cut -d ' ' -f3) \
   ${REF_FASTA} \
   ${FQ1} \
   ${FQ1/_1.fastq/_2.fastq} \
@@ -22,7 +22,7 @@ bwa-mem2 mem -M -t $THREADS -K 10000000 \
   && rm ${FQ1} && rm ${FQ1/_1.fastq/_2.fastq}
 else
 bwa-mem2 mem -M -t $THREADS -K 10000000 \
-  -R $(head -n1 ${RGFILE}) \
+  -R $(head -n $LSB_JOBINDEX ${INDIR}/infqfile.txt | tail -n1 | cut -d ' ' -f3) \
   ${REF_FASTA} \
   ${FQ1} \
   ${FQ1/_1.fastq/_2.fastq} \
