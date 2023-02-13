aadcda4f5503636744ce22fb267e9b57ed882e2f
Matt Johnson <matthew.j@wustl.edu>
2023-02-13 09:59:15 -0600

    Different GATK version to solve sample map problem

--- docker/wxsJoint/wxsjointcaller/scripts/jointcallinterval.bash
+++ docker/wxsJoint/wxsjointcaller/scripts/jointcallinterval.bash
@@ -4,7 +4,7 @@ TMP_DIR=${OUTDIR}/tmp${LSB_JOBINDEX} && mkdir ${TMP_DIR}
 DATABASE=$TMP_DIR/db
 rm -R $DATABASE
 INT_LISTS=($(ls /tmp | grep scattered))
-$GATK --java-options "-Xms200g -Xmx200g -XX:ConcGCThreads=2 -XX:ParallelGCThreads=2 -DGATK_STACKTRACE_ON_USER_EXCEPTION=true" \
+${GATK4261} --java-options "-Xms200g -Xmx200g -XX:ConcGCThreads=2 -XX:ParallelGCThreads=2 -DGATK_STACKTRACE_ON_USER_EXCEPTION=true" \
     GenomicsDBImport \
     -L /tmp/${INT_LISTS[${LSB_JOBINDEX}-1]} \
     --sample-name-map ${INDIR}/SampleMap.txt \
@@ -13,16 +13,14 @@ $GATK --java-options "-Xms200g -Xmx200g -XX:ConcGCThreads=2 -XX:ParallelGCThread
     --reader-threads 6 \
     --genomicsdb-vcf-buffer-size 65536 \
     --tmp-dir ${TMP_DIR} \
-    --genomicsdb-shared-posixfs-optimizations true \
-&& $GATK --java-options "-Xms200g -Xmx200g -XX:ConcGCThreads=2 -XX:ParallelGCThreads=2 -DGATK_STACKTRACE_ON_USER_EXCEPTION=true" \
+&& ${GATK4261} --java-options "-Xms200g -Xmx200g -XX:ConcGCThreads=2 -XX:ParallelGCThreads=2 -DGATK_STACKTRACE_ON_USER_EXCEPTION=true" \
     GenotypeGVCFs \
     -R ${REF_FASTA} \
     -V gendb://${DATABASE} \
     -O "${OUTDIR}/${LSB_JOBINDEX}.joint.vcf.gz" \
     -G StandardAnnotation \
     -G AS_StandardAnnotation \
-    --tmp-dir ${TMP_DIR} \
-    --genomicsdb-shared-posixfs-optimizations true 
+    --tmp-dir ${TMP_DIR}
 SUCCEEDED=$?
 rm -R $TMP_DIR
 exit $SUCCEEDED
