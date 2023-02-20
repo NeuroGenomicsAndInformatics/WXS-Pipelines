fc372ad5631dbb8f56d18436889db1255f4607e2
Matt Johnson <matthew.j@wustl.edu>
2023-02-20 13:00:33 -0600

    Remove user naming requirement

--- wrappers/fromCram.bash
+++ wrappers/fromCram.bash
@@ -1,13 +1,17 @@
 #!/bin/bash
 ## Needed for Parabricks
 export PATH="/opt/miniconda/bin:$PATH"
-REF_DIR="/scratch1/fs1/cruchagac/WXSref"
+REF_DIR="/scratch1/fs1/fernandezv/WXSref"
 
-[ ! -d /scratch1/fs1/cruchagac/${USER} ] && mkdir /scratch1/fs1/cruchagac/${USER}
-[ ! -d /scratch1/fs1/cruchagac/${USER}/c1in ] && mkdir /scratch1/fs1/cruchagac/${USER}/c1in
-[ ! -d /scratch1/fs1/cruchagac/${USER}/c1out ] && mkdir /scratch1/fs1/cruchagac/${USER}/c1out
-[ ! -d /storage1/fs1/cruchagac/Active/${USER}/c1out ] && mkdir //storage1/fs1/cruchagac/Active/${USER}/c1out
-[ ! -d /scratch1/fs1/cruchagac/${USER}/c1out/logs ] && mkdir /scratch1/fs1/cruchagac/${USER}/c1out/logs
+export COMPUTE_USER=fernandezv
+export SCRATCH_USER=cruchagac
+export STORAGE_USER=cruchagac
+
+[ ! -d /scratch1/fs1/${SCRATCH_USER}/${USER} ] && mkdir /scratch1/fs1/${SCRATCH_USER}/${USER}
+[ ! -d /scratch1/fs1/${SCRATCH_USER}/${USER}/c1in ] && mkdir /scratch1/fs1/${SCRATCH_USER}/${USER}/c1in
+[ ! -d /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out ] && mkdir /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out
+[ ! -d /storage1/fs1/${STORAGE_USER}/Active/${USER}/c1out ] && mkdir /storage1/fs1/${STORAGE_USER}/Active/${USER}/c1out
+[ ! -d /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out/logs ] && mkdir /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out/logs
 PRIORITY_ALIGN=60
 PRIORITY_BQSR=65
 PRIORITY_HC=70
@@ -16,12 +20,10 @@ PRIORITY_QC=50
 
 ## Set up directories and job submission variables
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-JOB_GROUP_C="/${USER}/compute-cruchagac"
-JOB_GROUP_F="/${USER}/compute-fernandezv"
-JOB_GROUP_GPU="/${USER}/compute-fernandezv/gpu"
-JOB_GROUP_ALIGN="/${USER}/compute-fernandezv/align"
-[[ -z "$(bjgroup | grep $JOB_GROUP_C)" ]] && bgadd -L 10 ${JOB_GROUP_C}
-[[ -z "$(bjgroup | grep $JOB_GROUP_F)" ]] && bgadd -L 300 ${JOB_GROUP_F}
+JOB_GROUP="/${USER}/compute-${COMPUTE_USER}"
+JOB_GROUP_GPU="/${USER}/compute-${COMPUTE_USER}/gpu"
+JOB_GROUP_ALIGN="/${USER}/compute-${COMPUTE_USER}/align"
+[[ -z "$(bjgroup | grep $JOB_GROUP)" ]] && bgadd -L 300 ${JOB_GROUP}
 [[ -z "$(bjgroup | grep $JOB_GROUP_GPU)" ]] && bgadd -L 10 ${JOB_GROUP_GPU}
 [[ -z "$(bjgroup | grep $JOB_GROUP_ALIGN)" ]] && bgadd -L 20 ${JOB_GROUP_ALIGN}
 
@@ -29,16 +31,15 @@ if [[ -f $1 ]]; then FULLSMIDS=($(cat $1)); else FULLSMIDS=($@); fi
 for FULLSMID in ${FULLSMIDS[@]}; do
 bash $HOME/WXS-Pipelines/makeSampleEnv.bash ${FULLSMID}
 JOBNAME="ngi-${USER}-${FULLSMID}"
-ENV_FILE=/scratch1/fs1/cruchagac/${USER}/c1in/envs/${FULLSMID}.env
-LOGDIR=/scratch1/fs1/cruchagac/${USER}/c1out/logs/${FULLSMID}
+ENV_FILE=/scratch1/fs1/${SCRATCH_USER}/${USER}/c1in/envs/${FULLSMID}.env
+LOGDIR=/scratch1/fs1/${SCRATCH_USER}/${USER}/c1out/logs/${FULLSMID}
 
 ## 2. BQSR
-LSF_DOCKER_VOLUMES="/scratch1/fs1/fernandezv:/scratch1/fs1/fernandezv \
-/scratch1/fs1/cruchagac:/scratch1/fs1/cruchagac \
+LSF_DOCKER_VOLUMES="/scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
 ${REF_DIR}:/ref \
 $HOME:$HOME" \
 LSF_DOCKER_ENV_FILE="$ENV_FILE" \
-bsub -g ${JOB_GROUP_F} \
+bsub -g ${JOB_GROUP} \
   -J ${JOBNAME}-bqsr \
   -w "done(\"${JOBNAME}-align\")" \
   -n 8 \
@@ -46,16 +47,15 @@ bsub -g ${JOB_GROUP_F} \
   -sp $PRIORITY_BQSR \
   -o ${LOGDIR}/${FULLSMID}.bqsr.%J.out \
   -R 'select[mem>50GB] rusage[mem=50GB] span[hosts=1]' \
-  -G compute-fernandezv \
+  -G compute-${COMPUTE_USER} \
   -q general \
   -a 'docker(mjohnsonngi/wxsrecalibrator:2.0)' \
   bash $SCRIPT_DIR/fixstage.bash\; \
   bash /scripts/bqsrspark.bash
 
 ## 3. Call Variants
-LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active:/storage1/fs1/cruchagac/Active \
-/scratch1/fs1/fernandezv:/scratch1/fs1/fernandezv \
-/scratch1/fs1/cruchagac:/scratch1/fs1/cruchagac \
+LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}/Active:/storage1/fs1/${STORAGE_USER}/Active \
+/scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
 /scratch1/fs1/ris/application/parabricks:/opt/parabricks \
 ${REF_DIR}:/ref \
 $HOME:$HOME" \
@@ -71,13 +71,13 @@ bsub -g ${JOB_GROUP_GPU} \
   -o ${LOGDIR}/${FULLSMID}.hc.%J.out \
   -R 'select[gpuhost && mem>180GB] rusage[mem=180GB] span[hosts=1]' \
   -gpu "num=1:gmem=16GB:j_exclusive=yes" \
-  -G compute-fernandezv \
+  -G compute-${COMPUTE_USER} \
   -q general \
   -a 'docker(mjohnsonngi/wxshaplotypecaller:2.0)' \
   bash /scripts/gpuhc.bash
 
-LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active:/storage1/fs1/cruchagac/Active \
-/scratch1/fs1/cruchagac:/scratch1/fs1/cruchagac \
+LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}/Active:/storage1/fs1/${STORAGE_USER}/Active \
+/scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
 ${REF_DIR}:/ref \
 $HOME:$HOME" \
 LSF_DOCKER_ENV_FILE=$ENV_FILE \
@@ -87,7 +87,7 @@ bsub -g ${JOB_GROUP_F} \
   -n1 \
   -sp 90 \
   -R 'rusage[mem=4GB]' \
-  -G compute-fernandezv \
+  -G compute-${COMPUTE_USER} \
   -q general \
   -a 'docker(mjohnsonngi/wxsstager:2.0)' \
   bash /scripts/stageout.bash
--- wrappers/fromRecal.bash
+++ wrappers/fromRecal.bash
@@ -1,13 +1,16 @@
 #!/bin/bash
 ## Needed for Parabricks
 export PATH="/opt/miniconda/bin:$PATH"
-REF_DIR="/scratch1/fs1/cruchagac/WXSref"
+REF_DIR="/scratch1/fs1/fernandezv/WXSref"
+export COMPUTE_USER=fernandezv
+export SCRATCH_USER=cruchagac
+export STORAGE_USER=cruchagac
 
-[ ! -d /scratch1/fs1/cruchagac/${USER} ] && mkdir /scratch1/fs1/cruchagac/${USER}
-[ ! -d /scratch1/fs1/cruchagac/${USER}/c1in ] && mkdir /scratch1/fs1/cruchagac/${USER}/c1in
-[ ! -d /scratch1/fs1/cruchagac/${USER}/c1out ] && mkdir /scratch1/fs1/cruchagac/${USER}/c1out
-[ ! -d /storage1/fs1/cruchagac/Active/${USER}/c1out ] && mkdir //storage1/fs1/cruchagac/Active/${USER}/c1out
-[ ! -d /scratch1/fs1/cruchagac/${USER}/c1out/logs ] && mkdir /scratch1/fs1/cruchagac/${USER}/c1out/logs
+[ ! -d /scratch1/fs1/${SCRATCH_USER}/${USER} ] && mkdir /scratch1/fs1/${SCRATCH_USER}/${USER}
+[ ! -d /scratch1/fs1/${SCRATCH_USER}/${USER}/c1in ] && mkdir /scratch1/fs1/${SCRATCH_USER}/${USER}/c1in
+[ ! -d /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out ] && mkdir /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out
+[ ! -d /storage1/fs1/${STORAGE_USER}/Active/${USER}/c1out ] && mkdir /storage1/fs1/${STORAGE_USER}/Active/${USER}/c1out
+[ ! -d /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out/logs ] && mkdir /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out/logs
 PRIORITY_ALIGN=60
 PRIORITY_BQSR=65
 PRIORITY_HC=70
@@ -16,12 +19,10 @@ PRIORITY_QC=50
 
 ## Set up directories and job submission variables
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-JOB_GROUP_C="/${USER}/compute-cruchagac"
-JOB_GROUP_F="/${USER}/compute-fernandezv"
-JOB_GROUP_GPU="/${USER}/compute-fernandezv/gpu"
-JOB_GROUP_ALIGN="/${USER}/compute-fernandezv/align"
-[[ -z "$(bjgroup | grep $JOB_GROUP_C)" ]] && bgadd -L 10 ${JOB_GROUP_C}
-[[ -z "$(bjgroup | grep $JOB_GROUP_F)" ]] && bgadd -L 300 ${JOB_GROUP_F}
+JOB_GROUP="/${USER}/compute-${COMPUTE_USER}"
+JOB_GROUP_GPU="/${USER}/compute-${COMPUTE_USER}/gpu"
+JOB_GROUP_ALIGN="/${USER}/compute-${COMPUTE_USER}/align"
+[[ -z "$(bjgroup | grep $JOB_GROUP)" ]] && bgadd -L 300 ${JOB_GROUP}
 [[ -z "$(bjgroup | grep $JOB_GROUP_GPU)" ]] && bgadd -L 10 ${JOB_GROUP_GPU}
 [[ -z "$(bjgroup | grep $JOB_GROUP_ALIGN)" ]] && bgadd -L 20 ${JOB_GROUP_ALIGN}
 
@@ -29,13 +30,12 @@ if [[ -f $1 ]]; then FULLSMIDS=($(cat $1)); else FULLSMIDS=($@); fi
 for FULLSMID in ${FULLSMIDS[@]}; do
 bash $HOME/WXS-Pipelines/makeSampleEnv.bash ${FULLSMID}
 JOBNAME="ngi-${USER}-${FULLSMID}"
-ENV_FILE=/scratch1/fs1/cruchagac/${USER}/c1in/envs/${FULLSMID}.env
-LOGDIR=/scratch1/fs1/cruchagac/${USER}/c1out/logs/${FULLSMID}
+ENV_FILE=/scratch1/fs1/${SCRATCH_USER}/${USER}/c1in/envs/${FULLSMID}.env
+LOGDIR=/scratch1/fs1/${SCRATCH_USER}/${USER}/c1out/logs/${FULLSMID}
 
 ## 3. Call Variants
-LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active:/storage1/fs1/cruchagac/Active \
-/scratch1/fs1/fernandezv:/scratch1/fs1/fernandezv \
-/scratch1/fs1/cruchagac:/scratch1/fs1/cruchagac \
+LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}/Active:/storage1/fs1/${STORAGE_USER}/Active \
+/scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
 /scratch1/fs1/ris/application/parabricks:/opt/parabricks \
 ${REF_DIR}:/ref \
 $HOME:$HOME" \
@@ -51,24 +51,24 @@ bsub -g ${JOB_GROUP_GPU} \
   -o ${LOGDIR}/${FULLSMID}.hc.%J.out \
   -R 'select[gpuhost && mem>180GB] rusage[mem=180GB] span[hosts=1]' \
   -gpu "num=1:gmem=16GB:j_exclusive=yes" \
-  -G compute-fernandezv \
+  -G compute-${COMPUTE_USER} \
   -q general \
   -a 'docker(mjohnsonngi/wxshaplotypecaller:2.0)' \
   bash $SCRIPT_DIR/fixstage.bash\; \
   bash /scripts/gpuhc.bash
 
-LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active:/storage1/fs1/cruchagac/Active \
-/scratch1/fs1/cruchagac:/scratch1/fs1/cruchagac \
+LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}/Active:/storage1/fs1/${STORAGE_USER}/Active \
+/scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
 ${REF_DIR}:/ref \
 $HOME:$HOME" \
 LSF_DOCKER_ENV_FILE=$ENV_FILE \
-bsub -g ${JOB_GROUP_F} \
+bsub -g ${JOB_GROUP} \
   -J ${JOBNAME}-stageout \
   -w "ended(\"${JOBNAME}-hc\")" \
   -n1 \
   -sp 90 \
   -R 'rusage[mem=4GB]' \
-  -G compute-fernandezv \
+  -G compute-${COMPUTE_USER} \
   -q general \
   -a 'docker(mjohnsonngi/wxsstager:2.0)' \
   bash /scripts/stageout.bash
