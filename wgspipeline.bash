8da6f25a820ff6700cc033db7b67de3706e9345a
Matt Johnson <matthew.j@wustl.edu>
2023-02-21 11:55:18 -0600

    Added explanation comments

--- wgspipeline.bash
+++ wgspipeline.bash
@@ -2,44 +2,63 @@
 ## Needed for Parabricks
 export PATH="/opt/miniconda/bin:$PATH"
 
-## Set up variables and update touch references
+## Set up variables for specific users. These should be all that's needed to change user
 export COMPUTE_USER=fernandezv
 export SCRATCH_USER=cruchagac
 export STORAGE_USER=cruchagac
-STORAGE_REF_DIR="/storage1/fs1/cruchagac/Active/matthew.j/REF/WXSref"
 export REF_DIR="/scratch1/fs1/fernandezv/WXSref"
+
+## Touch the references so that compute1 doesn't remove them
 find $REF_DIR -true -exec touch '{}' \;
 
+## 0. Set up for job submission 
+# 0.1 Make expected directories in case they are missing
 [ ! -d /scratch1/fs1/${SCRATCH_USER}/${USER} ] && mkdir /scratch1/fs1/${SCRATCH_USER}/${USER}
 [ ! -d /scratch1/fs1/${SCRATCH_USER}/${USER}/c1in ] && mkdir /scratch1/fs1/${SCRATCH_USER}/${USER}/c1in
 [ ! -d /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out ] && mkdir /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out
 [ ! -d /storage1/fs1/${STORAGE_USER}/Active/${USER}/c1out ] && mkdir /storage1/fs1/${STORAGE_USER}/Active/${USER}/c1out
 [ ! -d /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out/logs ] && mkdir /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out/logs
+
+# 0.2 Priorities are set to handle bounded-buffer issues
 PRIORITY_ALIGN=60
 PRIORITY_BQSR=65
 PRIORITY_HC=70
 PRIORITY_UTIL=80
 PRIORITY_QC=50
 
-## Set up directories and job submission variables
+# 0.3 Used to find other files needed in repository
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# 0.4 Define and create job groups
 JOB_GROUP="/${USER}/compute-${COMPUTE_USER}"
 JOB_GROUP_GPU="/${USER}/compute-${COMPUTE_USER}/gpu"
 JOB_GROUP_ALIGN="/${USER}/compute-${COMPUTE_USER}/align"
 JOB_GROUP_QC="/${USER}/compute-${COMPUTE_USER}/qc"
-[[ -z "$(bjgroup | grep $JOB_GROUP)" ]] && bgadd -L 300 ${JOB_GROUP_F}
+[[ -z "$(bjgroup | grep $JOB_GROUP)" ]] && bgadd -L 300 ${JOB_GROUP}
 [[ -z "$(bjgroup | grep $JOB_GROUP_GPU)" ]] && bgadd -L 10 ${JOB_GROUP_GPU}
 [[ -z "$(bjgroup | grep $JOB_GROUP_ALIGN)" ]] && bgadd -L 20 ${JOB_GROUP_ALIGN}
 [[ -z "$(bjgroup | grep $JOB_GROUP_QC)" ]] && bgadd -L 20 ${JOB_GROUP_QC}
 
+## Begin Job submission loop
+# This if checks if the submission was a workfile or a direct submission of a sample and adds them to an array
+# Cram inputs require a workfile, but bams and paired end fastqs can be run with only the FULLSMID
 if [[ -f $1 ]]; then FULLSMIDS=($(cat $1)); else FULLSMIDS=($@); fi
 for FULLSMID in ${FULLSMIDS[@]}; do
+# This script generates the .env environment file that all jobs use in the pipeline
+# Each sample has a unique environment; however, the script adds the reference variables that are the same per version
 bash ${SCRIPT_DIR}/makeSampleEnv.bash ${FULLSMID}
+# These 3 variables are used for each job submission to connect all the jobs for each sample consistent
 JOBNAME="ngi-${USER}-${FULLSMID}"
 ENV_FILE="/scratch1/fs1/${SCRATCH_USER}/${USER}/c1in/envs/$FULLSMID.env"
 LOGDIR=/scratch1/fs1/${SCRATCH_USER}/${USER}/c1out/logs/${FULLSMID}
 
 ## 1. Align
+# This job prepares data from all inputs to run as paired-end fastqs
+# This job produces an aligned, sorted, duplicate marked cram
+# The first alignment submission tries to run on a GPU, but settles for a CPU if one isn't available
+# The pipeline can be changed to only using GPU or CPU by modifying the -R submission flag
+# The -R flag structure is {GPU OPTIONS} || {CPU OPTIONS}@10
+# Removing the first or second half of the submission can force it to use GPU or CPU
 LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}/Active:/storage1/fs1/${STORAGE_USER}/Active \
 /scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
 /scratch1/fs1/ris/application/parabricks-license:/opt/parabricks \
@@ -60,7 +79,8 @@ bsub -g ${JOB_GROUP_ALIGN} \
   -a 'docker(mjohnsonngi/wxsaligner:2.0)' \
   bash /scripts/align.bash "$2"
 
-## Fallback if GPU fails
+## Fallback if GPU fails. 
+# If the first alignment job fails, usually due to a failure with a GPU, then this one will run
 LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}/Active:/storage1/fs1/${STORAGE_USER}/Active \
 /scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
 /scratch1/fs1/ris/application/parabricks-license:/opt/parabricks \
@@ -83,6 +103,10 @@ bsub -g ${JOB_GROUP_ALIGN} \
   bash /scripts/align.bash "$2"
 
 ## 2. BQSR
+# This job runs when either alignment job succeeds.
+# This job produces a BQSR report
+# The job itself runs BaseRecalibratorSpark on the cram created by the alignment job
+# The generated BQSR report is used by haplotypecaller without having to do ApplyBQSR
 LSF_DOCKER_VOLUMES="/scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
 ${REF_DIR}:/ref \
 $HOME:$HOME" \
@@ -101,6 +125,9 @@ bsub -g ${JOB_GROUP} \
   bash /scripts/bqsrspark.bash
 
 ## 3. Call Variants
+# This job uses the cram from the alignment staged and the BQSR report to call variants
+# This job produces a gvcf
+# This job utilizes GPUs to run haplotypecaller from NVIDIA's Clara Parabricks
 LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}/Active:/storage1/fs1/${STORAGE_USER}/Active \
 /scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
 /scratch1/fs1/ris/application/parabricks:/opt/parabricks \
@@ -125,6 +152,7 @@ bsub -g ${JOB_GROUP_GPU} \
   bash /scripts/gpuhc.bash
 
 ## 4. Stage out data
+# This job just moves data from scratch to storage and cleans up
 LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}/Active:/storage1/fs1/${STORAGE_USER}/Active \
 /scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
 $HOME:$HOME" \
@@ -142,6 +170,9 @@ bsub -g ${JOB_GROUP} \
     bash /scripts/stageout.bash\; sleep 100
 
 ## 5. QC
+# 5.1 Coverage
+# This job produces coverage reports for raw, high quality, and padded exome coverages
+# This job uses the pipeline-generated cram while it's on Active storage
 LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}/Active:/storage1/fs1/${STORAGE_USER}/Active \
 /scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
 ${REF_DIR}:/ref" \
@@ -158,6 +189,10 @@ bsub -g ${JOB_GROUP_QC} \
     -a 'docker(mjohnsonngi/wxscoverage:2.0)' \
     bash /scripts/get_all_wgsmetrics.bash
 
+# 5.2 FREEMIX
+# This job uses VerifyBamID2 to create a report on possible contamination for the sample
+# This job produces selfSM and Ancestry text files
+# This job uses the pipeline-generated cram while it's on Active storage
 LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}/Active:/storage1/fs1/${STORAGE_USER}/Active \
 /scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
 ${REF_DIR}:/ref" \
@@ -174,6 +209,9 @@ bsub -g ${JOB_GROUP_QC} \
     -a 'docker(mjohnsonngi/wxsfreemix:2.0)' \
     bash /scripts/vbid.bash
 
+## 5.3 Variant Calling Metrics
+# This job produces a variant calling metrics report that includes Ti/Tv ratios and #s of SNPs and INDELS
+# This job uses the pipeline-generated gvcf while it's on Active storage
 LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}/Active:/storage1/fs1/${STORAGE_USER}/Active \
 /scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
 ${REF_DIR}:/ref" \
@@ -190,6 +228,10 @@ bsub -g ${JOB_GROUP_QC} \
     -a 'docker(mjohnsonngi/wxsvariantmetrics:2.0)' \
     bash /scripts/gatkvcfmetrics.bash
 
+## 5.4 Key Gene Annotations
+# This job produces an annotation file using SnpEff
+# The genes annotated include APP, PSEN1, PSEN2, GRN, TREM2, and MAPT
+# This job uses the pipeline-generated gvcf while it's on Active storage
 LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}/Active:/storage1/fs1/${STORAGE_USER}/Active \
 /scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
 ${REF_DIR}:/ref" \
@@ -208,6 +250,9 @@ bsub -g ${JOB_GROUP_QC} \
     -a 'docker(mjohnsonngi/wxskeygeneannotator:2.0)' \
   	bash /scripts/keygene_annotate.bash
 
+## 5.5 Stats File
+# This job collects data from each of the previously generated reports into a single line
+# This job produces a csv with a header line and a line of data from the QC reports
 LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}/Active:/storage1/fs1/${STORAGE_USER}/Active \
 /scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
 $HOME:$HOME" \
