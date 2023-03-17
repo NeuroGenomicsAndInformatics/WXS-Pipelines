f300f9b128ffda000c11fd8bffc52ffaf8632208
Matt Johnson <matthew.j@wustl.edu>
2023-03-17 10:49:12 -0500

    Add plink1.9 wrapper script

---
+++ wrappers/plink19_wrapper.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+REF_DIR="/scratch1/fs1/fernandezv/WXSref"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+JOB_GROUP="/${USER}/compute-fernandezv"
+
+LSF_DOCKER_VOLUMES="/storage1/fs1/cruchagac/Active:/storage1/fs1/cruchagac/Active \
+${REF_DIR}:/ref" \
+bsub -g ${JOB_GROUP} \
+    -J ngi-${USER}-plink19 \
+    -n 8 \
+    -Ne \
+    -R 'rusage[mem=100GB]' \
+    -G compute-fernandezv \
+    -q general \
+    -a 'docker(biocontainers/plink1.9)' \
+    $@
