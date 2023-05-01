8d73441f7eca213aa312cbec931a3a2d7a201a53
Matt Johnson <matthew.j@wustl.edu>
2023-05-01 16:04:27 -0500

    Naming change to include interval

--- wrappers/jointwgsinterval_wrapper.bash
+++ wrappers/jointwgsinterval_wrapper.bash
@@ -38,7 +38,7 @@ LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}/Active:/storage1/fs1/${STORAGE
 $REF_DIR:/ref" \
 LSF_DOCKER_ENV_FILE=$ENV_FILE \
 bsub -g ${JOB_GROUP} \
-    -J ${JOBNAME}-call-rescue \
+    -J ${JOBNAME}-call-rescue-$INTERVAL \
     -Ne \
     -sp 90 \
     -n 2 \
