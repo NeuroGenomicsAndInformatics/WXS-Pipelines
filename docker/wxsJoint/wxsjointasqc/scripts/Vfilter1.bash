81759b023ec7a0036277dd591f8828bf44f67077
Matt Johnson <matthew.j@wustl.edu>
2023-05-15 10:00:55 -0500

    fix conditional

--- docker/wxsJoint/wxsjointasqc/scripts/Vfilter1.bash
+++ docker/wxsJoint/wxsjointasqc/scripts/Vfilter1.bash
@@ -46,7 +46,7 @@ echo "DP Filtered,${NAMEBASE}-PASS-maxDP${SNV_DP_TR}.vcf.gz,$(zcat ${NAMEBASE}-P
 	
 LCR="/scripts/LCR-hs38.bed"
 bedtools subtract -header -A -a ${NAMEBASE}-PASS-maxDP${SNV_DP_TR}.vcf.gz -b ${LCR} | bgzip -c > ${NAMEBASE}-PASS-maxDP${SNV_DP_TR}-LCR.vcf.gz 
-bcftools index -t --threads $LSB_MAX_NUM_PROCESSORS ${NAMEBASE}-PASS-maxDP${SNV_DP_TR}-LCR.vcf.gz
+bcftools index -t --threads $LSB_MAX_NUM_PROCESSORS ${NAMEBASE}-PASS-maxDP${SNV_DP_TR}-LCR.vcf.gz \
 && rm ${NAMEBASE}-PASS-maxDP${SNV_DP_TR}-LCR.vcf.gz
 
 
