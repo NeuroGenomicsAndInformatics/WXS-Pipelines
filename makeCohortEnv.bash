78c72c7935b1a9bb0b7cf444800558cb9c96815c
Matt Johnson <matthew.j@wustl.edu>
2023-05-22 10:39:44 -0500

    Environment script cleanup

--- makeCohortEnv.bash
+++ makeCohortEnv.bash
@@ -9,8 +9,6 @@ echo -e "COHORT=${COHORT}" > $ENV_FILE
 echo -e "CHR=${CHR}" >> $ENV_FILE
 echo -e "INDIR=/storage1/fs1/${STORAGE_USER}/Active/${USER}/c1in/${COHORT}" >> $ENV_FILE
 [ ! -d /storage1/fs1/${STORAGE_USER}/Active/${USER}/c1out/${COHORT}_${CHR}_work ] && mkdir /storage1/fs1/${STORAGE_USER}/Active/${USER}/c1out/${COHORT}_${CHR}_work
-[ ! -d /storage1/fs1/${STORAGE_USER}/Active/${USER}/c1out/${COHORT}_${CHR}_work/joint_vcfs ] && mkdir /storage1/fs1/${STORAGE_USER}/Active/${USER}/c1out/${COHORT}_${CHR}_work/joint_vcfs
-[ ! -d /storage1/fs1/${STORAGE_USER}/Active/${USER}/c1out/${COHORT}_${CHR}_work/dbs ] && mkdir /storage1/fs1/${STORAGE_USER}/Active/${USER}/c1out/${COHORT}_${CHR}_work/dbs
 echo -e "OUTDIR=/storage1/fs1/${STORAGE_USER}/Active/${USER}/c1out/${COHORT}_${CHR}_work" >> $ENV_FILE
 [ ! -d /storage1/fs1/${STORAGE_USER}/Active/${USER}/c1out/${COHORT}_${CHR} ] && mkdir /storage1/fs1/${STORAGE_USER}/Active/${USER}/c1out/${COHORT}_${CHR}
 echo -e "FINAL_OUTDIR=/storage1/fs1/${STORAGE_USER}/Active/${USER}/c1out/${COHORT}_${CHR}" >> $ENV_FILE
