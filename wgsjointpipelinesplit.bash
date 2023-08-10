b466baeda54bd9f187988496cd6d31507fc5b97e
Matt Johnson <matthew.j@wustl.edu>
2023-08-10 11:29:29 -0500

    Better comments

--- wgsjointpipelinesplit.bash
+++ wgsjointpipelinesplit.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
-## This script runs the joint pipeline on a single chromosome
-# The two arguments are the COHORT and the CHR to be run
+## This script runs the joint pipeline on a the entire genome by splitting it into intervals
+# The two arguments are the COHORT and the NUM_INTERVALS to split the genome into
 # The COHORT argument is the name of the location in /storage1/fs1/${STORAGE_USER}/Active/$USER/c1in
-# The CHR argument is the chromosome to be run (ex. chr1, chrX)
+# The NUM_INTERVALS argument is the number of intervals to split the genome into
 COHORT=$1
 NUM_INTERVALS=$2
 
@@ -21,16 +21,15 @@ JOB_GROUP="/${USER}/compute-${COMPUTE_USER}/joint"
 [[ -z "$(bjgroup | grep $JOB_GROUP)" ]] && bgadd -L 50 ${JOB_GROUP}
 [ ! -d /scratch1/fs1/${COMPUTE_USER}/${USER}/c1out/logs ] && mkdir /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out/logs
 
-# This component is to find the number of jobs to use in an array for calling
-# The number echoed represents the number of intervals in the interval list for that CHR
-echo $NUM_INTERVALS
-
+# The first calling stage is looped to run all of the splits
+# This script doesn't use job arrays because of the max of 1000 jobs in an array
 for ((i=0 ; i < $NUM_INTERVALS ; i++)); do
 
-## 1a. Joint Call on Intervals - genomicsDB
-# This first job is an array of jobs based on the number of intervals in the interval list for the CHR given
+## 1a. Joint Call on Intervals - genomicsDB and GenotypeGVCFs
+# This first job is split into the number of intervals with each interval having its own job
 # These jobs each add the intervals from all input gvcfs to a genomicsdb.
-# The outcome from this step should be a genomicsDB for each interval
+# After the genomicsDBImport is completed, the job calls variants with GenotypeGVCFs
+# This job results in a single vcf for the interval given
 LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}/Active:/storage1/fs1/${STORAGE_USER}/Active \
 /scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
 $REF_DIR:/ref" \
