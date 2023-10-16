93735bedd904f0a21d42cea21bda86d9caebdb7c
Matt Johnson <matthew.j@wustl.edu>
2023-10-16 12:49:56 -0500

    Fix not accepting local intlist

--- docker/wxsJoint/wxsjointasqc/scripts/splitintsdivide.bash
+++ docker/wxsJoint/wxsjointasqc/scripts/splitintsdivide.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 $GATK SplitIntervals \
     -R $REF_FASTA \
-    -L /intlists/WGS.interval_list \
+    -L /scratch1/fs1/cruchagac/WXSref/20190812_GATK_38_googlebundle/resources_broad_hg38_v0_wgs_calling_regions.hg38.interval_list \
     -O /tmp \
     --scatter-count $NUM_INTERVALS \
     --interval-merging-rule OVERLAPPING_ONLY \
