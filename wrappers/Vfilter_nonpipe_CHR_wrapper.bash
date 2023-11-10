5b02797ba1d0744347b138ee4aaf03cac96dec25
Matt Johnson <matthew.j@wustl.edu>
2023-11-10 09:51:12 -0600

    wrapper for split ADSPR4

---
+++ wrappers/Vfilter_nonpipe_CHR_wrapper.bash
@@ -0,0 +1,43 @@
+#!/bin/bash
+## This script runs the joint pipeline on a the entire genome by splitting it into intervals
+# The two arguments are the COHORT and the NUM_INTERVALS to split the genome into
+# The COHORT argument is the name of the location in /storage1/fs1/${STORAGE_USER}/Active/$USER/c1in
+# The NUM_INTERVALS argument is the number of intervals to split the genome into
+COHORT=$1
+NUM_INTERVALS=$2
+RECAL_VCF=$3
+CHR=$4
+
+# These variables can be changed to run for other users
+export COMPUTE_USER=fernandezv
+export STORAGE_USER=cruchagac
+export SCRATCH_USER=cruchagac
+REF_DIR="/scratch1/fs1/cruchagac/WXSref"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ENV_FILE=$(bash ${SCRIPT_DIR}/../makeCohortEnv5.bash $COHORT $NUM_INTERVALS)
+
+# Pipeline variable setup for running the jobs
+JOBNAME="ngi-${USER}-${COHORT}-${CHR}"
+JOB_GROUP="/${USER}/compute-${COMPUTE_USER}/joint"
+[[ -z "$(bjgroup | grep $JOB_GROUP)" ]] && bgadd -L 50 ${JOB_GROUP}
+[ ! -d /scratch1/fs1/${COMPUTE_USER}/${USER}/c1out/logs ] && mkdir /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out/logs
+
+## 3. Joint QC
+# This step performs VQSR filtering and a host of other filters on the CHR joint vcf
+# The outcome from this step is a file containing counts of the variants in each step and a final filtered joing vcf for the CHR
+LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}/Active:/storage1/fs1/${STORAGE_USER}/Active \
+/scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
+$REF_DIR:/ref" \
+LSF_DOCKER_ENV_FILE=$ENV_FILE \
+bsub -g ${JOB_GROUP} \
+    -J ${JOBNAME}-qc[1-50] \
+    -Ne \
+    -n 2 \
+    -sp 90 \
+    -o /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out/logs/${COHORT}.joint_s3.%J.out \
+    -R 'select[mem>100GB] rusage[mem=100GB] span[hosts=1]' \
+    -G compute-${COMPUTE_USER} \
+    -q general \
+    -a 'docker(mjohnsonngi/wxsjointasqc:2.0)' \
+    bash /scripts/Vfilter1_nonpipe.bash $RECAL_VCF $CHR
