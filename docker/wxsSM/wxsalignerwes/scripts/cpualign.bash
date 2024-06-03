6ebca1a1a108a0464eb6c555d16bba16795bbe90
Matt Johnson <matthew.j@wustl.edu>
2024-06-03 16:03:27 -0500

    Fix bwait failure

--- docker/wxsSM/wxsalignerwes/scripts/cpualign.bash
+++ docker/wxsSM/wxsalignerwes/scripts/cpualign.bash
@@ -4,7 +4,7 @@ LSF_DOCKER_ENV_FILE="$ENV_FILE" \
 bsub -q general \
   -g /matthew.j/compute-${COMPUTE_USER} \
   -G compute-${COMPUTE_USER} \
-  -J ngi-$USER-$FULLSMID-fqsalign[1-$(( JOBS_IN_ARRAY - 1 ))] \
+  -J fqsalign${LSB_JOBID}[1-$(( JOBS_IN_ARRAY - 1 ))] \
   -n 4 \
   -sp 55 \
   -o ${LOGDIR}/align_${FULLSMID}.%J.%I.out \
@@ -12,5 +12,15 @@ bsub -q general \
   -a 'docker(mjohnsonngi/wxsalignhelper:2.0)' \
   bash /scripts/bwa_helperfqs4wes.bash
 bash /scripts/bwa_helperfqs4base.bash
-[[ $JOBS_IN_ARRAY -eq 1 ]] || bwait -w "done(\"ngi-$USER-$FULLSMID-fqsalign\")" || exit 67
+bsub -q general \
+  -g /matthew.j/compute-${COMPUTE_USER} \
+  -G compute-${COMPUTE_USER} \
+  -w "done(fqsalign${LSB_JOBID})" \
+  -n 1 \
+  -Ne \
+  -K \
+  -sp 55 \
+  -a 'docker(mjohnsonngi/wxsalignhelper:2.0)' \
+  'echo "hello" > ${INDIR}/holder_exit.txt'
+[[ $JOBS_IN_ARRAY -eq 1 ]] || [[ -f /${INDIR}/holder_exit.txt ]] || exit 67
 bash /scripts/md_helper.bash
