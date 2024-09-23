c9eb346af5568cf1f28bf7459539a614e8275c47
Matt Johnson <matthew.j@wustl.edu>
2024-09-23 11:14:29 -0500

    Updating to parabricks 4.3

--- docker/wxsSM/wxsaligner/scripts/gpualign.bash
+++ docker/wxsSM/wxsaligner/scripts/gpualign.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
-pbrun fq2bam \
+/usr/local/parabricks/pbrun fq2bam \
     --ref ${REF_FASTA} \
     --in-fq-list ${INDIR}/infqfile.txt \
     --out-bam ${INDIR}/${BAM} \
     --out-duplicate-metrics ${OUTDIR}/${FULLSMID}.dup.metrics.txt \
     --num-gpus 1 \
+    --memory-limit 200 \
     --tmp-dir ${TMP_DIR} \
     || (rm -R $INDIR && exit 66)
 samtools view -@ $LSB_MAX_NUM_PROCESSORS -C -T ${REF_FASTA} -o $OUTDIR/$CRAM $INDIR/$BAM
