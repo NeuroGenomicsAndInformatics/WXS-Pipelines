fd186d8111094adab631d6e6514339a026ded7c6
Matt Johnson <matthew.j@wustl.edu>
2025-01-07 10:56:44 -0600

    Add R5 wrappers

---
+++ wrappers/ADSPR5/R5_sites.bash
@@ -0,0 +1,58 @@
+#!/bin/bash
+## Set up variables for specific users. These should be all that's needed to change user
+export COMPUTE_USER=fernandezv
+export SCRATCH_USER=cruchagac
+export STORAGE_USER=cruchagac
+export REF_DIR="/scratch1/fs1/cruchagac/WXSref"
+
+## Touch the references so that compute1 doesn't remove them
+find $REF_DIR -true -exec touch '{}' \;
+
+## 0. Set up for job submission 
+# 0.1 Make expected directories in case they are missing
+[ ! -d /scratch1/fs1/${SCRATCH_USER}/${USER} ] && mkdir /scratch1/fs1/${SCRATCH_USER}/${USER}
+[ ! -d /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out ] && mkdir /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out
+[ ! -d /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out/logs ] && mkdir /scratch1/fs1/${SCRATCH_USER}/${USER}/c1out/logs
+
+# 0.2 Priorities are set to handle bounded-buffer issues
+PRIORITY_UTIL=55
+
+# 0.3 Used to find other files needed in repository
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# 0.4 Define and create job groups
+JOB_GROUP="/${USER}/compute-${COMPUTE_USER}"
+JOB_GROUP_JOINT="/${USER}/compute-${COMPUTE_USER}/joint"
+[[ -z "$(bjgroup | grep $JOB_GROUP)" ]] && bgadd -L 500 ${JOB_GROUP}
+[[ -z "$(bjgroup | grep $JOB_GROUP_JOINT)" ]] && bgadd -L 480 ${JOB_GROUP_JOINT}
+
+## Begin Job submission
+# Set up input
+INPUT_VCF=$1
+INTERVAL=$2
+NAMEBASE=$(echo ${INPUT_VCF##*/} | cut -d. -f1)
+
+# These 3 variables are used for each job submission to connect all the jobs for each sample consistent
+JOBNAME="ngi-${USER}-${NAMEBASE}"
+ENV_FILE="${SCRIPT_DIR}/baseEnvs/references_2_0.env"
+LOGDIR=/scratch1/fs1/${SCRATCH_USER}/${USER}/c1out/logs/${NAMEBASE}
+[[ -d $LOGDIR ]] || mkdir $LOGDIR
+
+# 1.3 Make sites-only vcf
+# This job produces a sites-only vcf file.
+LSF_DOCKER_VOLUMES="/storage1/fs1/${STORAGE_USER}/Active:/storage1/fs1/${STORAGE_USER}/Active \
+/scratch1/fs1/${SCRATCH_USER}:/scratch1/fs1/${SCRATCH_USER} \
+${REF_DIR}:/ref \
+$HOME:$HOME" \
+LSF_DOCKER_ENV_FILE="${ENV_FILE}" \
+bsub -g ${JOB_GROUP_JOINT} \
+  -J ${JOBNAME}-SITES-${INTERVAL} \
+  -n 1 \
+  -Ne \
+  -o ${LOGDIR}/${NAMEBASE}.sites.%J.${INTERVAL}.out \
+  -R '{ select[mem>20GB] rusage[mem=20GB] }' \
+  -G compute-${COMPUTE_USER} \
+  -q general \
+  -sp $PRIORITY_UTIL \
+  -a 'docker(mjohnsonngi/wxsjointqc:2.0)' \
+  bash /scripts/make_sites_only_vcf.bash ${INPUT_VCF%/*} ${INTERVAL}
